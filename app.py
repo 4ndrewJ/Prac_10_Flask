3e50f0c23c135484ff5226321d3453ca19340ddf
AndrewJ <andrew.james3@my.jcu.edu.au>
2021-10-24 12:38:58 +1000

    Add completed code for prac 10 flask project

--- app.py
+++ app.py
@@ -5,7 +5,35 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return '<h1>Hello World :)</h1>'
+
+
+@app.route('/greet')
+@app.route('/greet/<name>')
+def greet(name=''):
+    return f'Hello {name}'
+
+
+@app.route('/f')
+@app.route('/f/<celsius>')
+def convert_to_fahrenheit(celsius=0):
+    result = str(convert_celsius_to_fahrenheit(float(celsius)))
+    return f'{celsius} degrees celsius is {result} degrees fahrenheit'
+
+
+@app.route('/c')
+@app.route('/c/<fahrenheit>')
+def convert_to_celsius(fahrenheit=0):
+    result = str(convert_fahrenheit_to_celsius(float(fahrenheit)))
+    return f'{fahrenheit} degrees fahrenheit is {result} degrees celsius'
+
+
+def convert_fahrenheit_to_celsius(fahrenheit):
+    return (5 / 9) * (fahrenheit - 32)
+
+
+def convert_celsius_to_fahrenheit(celsius):
+    return celsius * 9.0 / 5 + 32
 
 
 if __name__ == '__main__':
